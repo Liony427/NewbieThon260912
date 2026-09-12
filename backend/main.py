395983441da683099648fbccfcb58ed168a2152e
JaeHyeon <jhlim0427@naver.com>
2026-09-12 13:42:03 +0900

    13:41임재현_예약 조회 및 1대1 매칭 기능 구현

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from backend.database import Base, engine
 from backend import models
+from backend.routers import matching
 
 
 Base.metadata.create_all(bind=engine)
@@ -11,6 +12,9 @@ app = FastAPI(
     title="자전거 바톤터치 API",
     version="0.1.0"
 )
+
+
+app.include_router(matching.router)
 
 
 @app.get("/")
